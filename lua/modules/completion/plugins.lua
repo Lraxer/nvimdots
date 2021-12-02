9dad3ca3c0647e61251da662c68ae5d0598a5495
Lraxer <792296704@qq.com>
2021-12-02 17:12:14 +0800

    comment copilot

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -54,6 +54,7 @@ completion["windwp/nvim-autopairs"] = {
 	after = "nvim-cmp",
 	config = conf.autopairs,
 }
-completion["github/copilot.vim"] = { opt = true, cmd = "Copilot" }
+
+-- completion["github/copilot.vim"] = {opt = true, cmd = "Copilot"}
 
 return completion
