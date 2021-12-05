caabeb1fcf7e86417f90aab1dd78f713fa68991a
Lraxer <792296704@qq.com>
2021-12-05 23:03:26 +0800

    Set better-escape time interval

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -45,6 +45,8 @@ local disable_distribution_plugins = function()
 	vim.g.did_load_netrwSettings = 1
 	vim.g.did_load_netrwFileHandlers = 1
 	vim.g.better_escape_shortcut = 'jj'
+	vim.g.better_escape_shortcut = 'jj' 
+	vim.g.better_escape_interval = 400
 end
 
 local leader_map = function()
